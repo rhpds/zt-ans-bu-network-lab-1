004afbf6cf6b4b0e6c13efc734631dc6de3a9ee9
Mitesh Sharma <mitsharm@redhat.com>
2025-09-16 10:24:54 +0530

    update

--- runtime-automation/module-03/solve-controller.sh
+++ runtime-automation/module-03/solve-controller.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-cat > /tmp/setup-scripts/solve_challenege_3.yml << EOF
-- name: solve challenge 3
-  hosts: localhost
-  connection: local
-  gather_facts: false
-  collections:
-    - ansible.controller
-  vars:
-    aap_hostname: localhost
-    aap_username: admin
-    aap_password: ansible123!
-    aap_validate_certs: false
-  tasks:
-    - name: Launch Network Automation - Restore
-      ansible.controller.job_launch:
-        job_template: "Network Automation - Restore"
-        controller_username: "{{ aap_username }}"
-        controller_password: "{{ aap_password }}"
-        controller_host: "https://{{ aap_hostname }}"
-        validate_certs: "{{ aap_validate_certs }}"
-      register: job
+# cat > /tmp/setup-scripts/solve_challenege_3.yml << EOF
+# - name: solve challenge 3
+#   hosts: localhost
+#   connection: local
+#   gather_facts: false
+#   collections:
+#     - ansible.controller
+#   vars:
+#     aap_hostname: localhost
+#     aap_username: admin
+#     aap_password: ansible123!
+#     aap_validate_certs: false
+#   tasks:
+#     - name: Launch Network Automation - Restore
+#       ansible.controller.job_launch:
+#         job_template: "Network Automation - Restore"
+#         controller_username: "{{ aap_username }}"
+#         controller_password: "{{ aap_password }}"
+#         controller_host: "https://{{ aap_hostname }}"
+#         validate_certs: "{{ aap_validate_certs }}"
+#       register: job
 
-EOF
-sudo su - -c "ANSIBLE_COLLECTIONS_PATH=/root/.ansible/collections/ansible_collections/ /usr/bin/ansible-playbook /tmp/setup-scripts/solve_challenege_3.yml"
+# EOF
+# sudo su - -c "ANSIBLE_COLLECTIONS_PATH=/root/.ansible/collections/ansible_collections/ /usr/bin/ansible-playbook /tmp/setup-scripts/solve_challenege_3.yml"
--- runtime-automation/module-03/validation-controller.sh
+++ runtime-automation/module-03/validation-controller.sh
@@ -1,81 +1,71 @@
 #!/bin/bash
 
-tower-cli config verify_ssl false
-tower-cli login admin --password ansible123!
+# cat >/tmp/setup-scripts/check_challenege_3.yml << EOF
+# ---
+# - name: setup controller for network use cases
+#   hosts: localhost
+#   connection: local
+#   gather_facts: false
+#   collections:
+#     - ansible.controller
+#   vars:
+#     aap_hostname: localhost
+#     aap_username: admin
+#     aap_password: ansible123!
+#     aap_validate_certs: false
+#   tasks:
+#     - name: Get job templates from Automation Controller
+#       uri:
+#         url: https://{{ aap_hostname }}/api/controller/v2/job_templates/
+#         method: GET
+#         validate_certs: "{{ aap_validate_certs }}"
+#         user: "{{ aap_username }}"
+#         password: "{{ aap_password}}"
+#         force_basic_auth: yes
+#       register: job_templates
 
-test1output=$(tower-cli job list -f json -J "Network Automation - Restore" | jq '.count');
-
-if [[ "$test1output" == "0" ]]; then
-    echo "You have not launched the 'Network Automation - Restore' job template"
-    exit 1
-fi
-
-cat >/tmp/setup-scripts/check_challenege_3.yml << EOF
----
-- name: setup controller for network use cases
-  hosts: localhost
-  connection: local
-  gather_facts: false
-  collections:
-    - ansible.controller
-  vars:
-    aap_hostname: localhost
-    aap_username: admin
-    aap_password: ansible123!
-    aap_validate_certs: false
-  tasks:
-    - name: Get job templates from Automation Controller
-      uri:
-        url: https://{{ aap_hostname }}/api/controller/v2/job_templates/
-        method: GET
-        validate_certs: "{{ aap_validate_certs }}"
-        user: "{{ aap_username }}"
-        password: "{{ aap_password}}"
-        force_basic_auth: yes
-      register: job_templates
-
-    - name: Print job template names
-      debug:
-        msg: "{{ job_templates.json.results | map(attribute='name') | list }}"
+#     - name: Print job template names
+#       debug:
+#         msg: "{{ job_templates.json.results | map(attribute='name') | list }}"
     
-    - name: Extract job template names
-      set_fact:
-        template_names: "{{ job_templates.json.results | map(attribute='name') | list }}"
+#     - name: Extract job template names
+#       set_fact:
+#         template_names: "{{ job_templates.json.results | map(attribute='name') | list }}"
 
-    - name: Fail template Network Automation - Backup is not found
-      fail:
-        msg: "Job template 'Network Automation - Backup' does not exist in Automation Controller!"
-      when: "'Network Automation - Backup' not in template_names"
+#     - name: Fail template Network Automation - Backup is not found
+#       fail:
+#         msg: "Job template 'Network Automation - Backup' does not exist in Automation Controller!"
+#       when: "'Network Automation - Backup' not in template_names"
 
-    - name: Fail template Network Automation - Restore is not found
-      fail:
-        msg: "Job template 'Network Automation - Restore' does not exist in Automation Controller!"
-      when: "'Network Automation - Restore' not in template_names"
+#     - name: Fail template Network Automation - Restore is not found
+#       fail:
+#         msg: "Job template 'Network Automation - Restore' does not exist in Automation Controller!"
+#       when: "'Network Automation - Restore' not in template_names"
 
-    - name: Get job templates from Automation Controller
-      uri:
-        url: https://{{ aap_hostname }}/api/controller/v2/jobs/
-        method: GET
-        validate_certs: "{{ aap_validate_certs }}"
-        user: "{{ aap_username }}"
-        password: "{{ aap_password}}"
-        force_basic_auth: yes
-      register: jobs
+#     - name: Get job templates from Automation Controller
+#       uri:
+#         url: https://{{ aap_hostname }}/api/controller/v2/jobs/
+#         method: GET
+#         validate_certs: "{{ aap_validate_certs }}"
+#         user: "{{ aap_username }}"
+#         password: "{{ aap_password}}"
+#         force_basic_auth: yes
+#       register: jobs
 
-    - name: Extract job names
-      set_fact:
-        job_names: "{{ jobs.json.results | map(attribute='name') | list }}"
+#     - name: Extract job names
+#       set_fact:
+#         job_names: "{{ jobs.json.results | map(attribute='name') | list }}"
 
-    - name: Fail Job Network Automation - Restore is not found
-      fail:
-        msg: "Job template 'Network Automation - Restore' does not exist in Automation Controller!"
-      when: "'Network Automation - Restore' not in job_names"
+#     - name: Fail Job Network Automation - Restore is not found
+#       fail:
+#         msg: "Job template 'Network Automation - Restore' does not exist in Automation Controller!"
+#       when: "'Network Automation - Restore' not in job_names"
 
-EOF
+# EOF
 
-/usr/bin/ansible-playbook /tmp/setup-scripts/check_challenege_3.yml
+# /usr/bin/ansible-playbook /tmp/setup-scripts/check_challenege_3.yml
 
-if [ $? -ne 0 ]; then
-    echo "You have not launched the 'Network Automation - Restore' job template"
-    exit 1
-fi
+# if [ $? -ne 0 ]; then
+#     echo "You have not launched the 'Network Automation - Restore' job template"
+#     exit 1
+# fi
